8c68784d465b203b5d088064bf95ab470bf68d15
Zaibbee69 <jahanzaib12569@gmail.com>
2025-10-16 17:28:40 +0500

    Ok here goes nothing rn my app works

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 import LandingPage from "./pages/LandingPage.js";
 import IndexPage from "./pages/IndexPage.js";
+import ApiHandler from "./api/ApiHandler.js";
 
 const App = (function () {
 
-    let pageSwitch = true;
+    let location, date, currentTemp, currentConditions, currentWind, currentIcon, currentHumidity, daysData, hoursData;
+    let pageSwitch = false;
     const generateLandingPage = () => LandingPage.init();
     const generateIndexPage = (location, date, currentTemp, currentConditions, currentWind, currentHumidity, daysData, hoursData, currentIcon) =>
         IndexPage.init(location, date, currentTemp, currentConditions, currentWind, currentHumidity, daysData, hoursData, currentIcon);
 
+    const showWeather = async () => {
+        const weatherData = await ApiHandler.makeApiCall();
+        console.log("Time:", weatherData.daysData[0].hours)
+        location = weatherData.location;
+        date = weatherData.daysData[0].date;
+        currentTemp = weatherData.currentData.temperature;
+        currentConditions = weatherData.daysData[0].conditions;
+        currentWind = weatherData.currentData.windSpeed;
+        currentIcon = weatherData.currentData.icon;
+        currentHumidity = weatherData.currentData.humidity;
+        daysData = weatherData.daysData;
+        hoursData = weatherData.daysData[0].hours;
+
+        generateIndexPage(location, date, currentTemp, currentConditions, currentWind, currentHumidity, daysData, hoursData, currentIcon);
+    }
 
     const switchPages = () => {
         if (!pageSwitch) {
--- pages/IndexPage.js
+++ pages/IndexPage.js
@@ -1,6 +1,3 @@
-import ApiHandler from "./api/ApiHandler.js";
-
-
 import LocationHeader from "../components/LocationHeader.js";
 import CurrentDaySection from "../components/CurrentDaySection.js";
 import WeekSection from "../components/WeekSection.js";
@@ -8,23 +5,6 @@ import HoursSection from "../components/HoursSection.js";
 import WeatherImgSection from "../components/WeatherImgSection.js";
 
 const IndexPage = (function () {
-
-    let location, date, currentTemp, currentConditions, currentWind, currentIcon, currentHumidity, daysData, hoursData;
-
-    const showWeather = async () => {
-        const weatherData = await ApiHandler.makeApiCall();
-        location = weatherData.location;
-        date = weatherData.daysData[0].date;
-        currentTemp = weatherData.currentData.temperature;
-        currentConditions = weatherData.daysData[0].conditions;
-        currentWind = weatherData.currentData.windSpeed;
-        currentIcon = weatherData.currentData.icon;
-        currentHumidity = weatherData.currentData.humidity;
-        daysData = weatherData.daysData;
-        hoursData = weatherData.daysData[0].hours;
-
-        init(location, date, currentTemp, currentConditions, currentWind, currentHumidity, daysData, hoursData, currentIcon);
-    }
 
 
     const removeIndexPage = () => {
